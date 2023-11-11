37298c90be13056fb64380c4f06417fb1568d6ea
Devapriya Reghu <118672654+devarx-dev@users.noreply.github.com>
2023-11-11 22:48:52 +0530

    Add files via upload

---
+++ server.mjs
@@ -0,0 +1,40 @@
+
+import * as http from 'http';
+var fs=require('fs')
+
+
+function server(req, res) {
+
+if (req.url==='/'){
+
+    fs.readFile('samplehtml.html', function (err,data){
+    
+    res.writeHead (200, {'Content-Type': 'text/html'})
+     res.write(hai)
+    
+    res.end()
+    
+    })
+}
+    else if(req.url==='/login')
+    { 
+        res.write('login')
+    
+    res.end()
+    
+    }
+    else{
+    
+    res.writeHead(404, {'Content-Type': 'text/html'})
+    res.write('error')
+    }
+    I
+    
+    res.end()
+    
+    
+}
+// Create an HTTP server and listen on port 8000
+http.createServer(server).listen(8000, () => {
+    console.log('Server is running on http://localhost:8000/');
+});
